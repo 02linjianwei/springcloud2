7e9fb5d6fd67f390c2e47b8531581d1d9b4a3a18
02linjianwei <51610618+02linjianwei@users.noreply.github.com>
2021-04-11 12:03:52 +0800

    Delete src directory

--- src/main/java/com/example/demo/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.domain.Role;
-
-public interface RoleRepository extends BaseRepository<Role,Long> {
-
-}
--- src/main/java/com/example/demo/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.domain.User;
-
-public interface UserRepository extends BaseRepository<User,Long>{
-    User findByName(String name);
-}
